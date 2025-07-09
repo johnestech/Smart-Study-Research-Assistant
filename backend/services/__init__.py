36771f9032d673e84ed1253c3f1b965a2b8e8d9b
John Ebuga Saduna <87089715+johnestech@users.noreply.github.com>
2025-07-09 05:41:05 +0100

    services init file

---
+++ backend/services/__init__.py
@@ -0,0 +1,5 @@
+from .supabase_client import supabase_service
+from .gemini_client import gemini_service
+from .file_processor import file_processor
+
+__all__ = ['supabase_service', 'gemini_service', 'file_processor']
